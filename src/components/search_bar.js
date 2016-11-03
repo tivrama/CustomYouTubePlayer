e3dee4c87ae8251e4f97b4681bed2f4cf7ec9ce2
tivrama <tivrama@yahoo.com>
2016-11-03 12:51:50 -0700

    First commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render () {
+    return (
+      <div className='search-bar'>
+        <input 
+          value={this.state.term}
+          onChange={ event => this.onInputChange(event.target.value) } />
+      </div>
+    );
+  }
+
+  onInputChange(term) {
+    console.log('term: ', term);
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+  }
+
+}
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render } from 'react-dom';
+import _ from 'lodash';
+import TYSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyDIjN94tBazDmhk0-K_NAd2vbsHmVHq7hQ';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('wierd al');
+  }
+
+  videoSearch(term) {
+    TYSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render () {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+};
+
+render(<App />, document.querySelector('.container'));
