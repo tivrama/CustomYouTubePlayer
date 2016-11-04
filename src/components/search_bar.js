d68e7e9e0bd5bd9235901cee07b9bf3a4959db2a
tivrama <tivrama@yahoo.com>
2016-11-04 12:32:27 -0700

    p.json

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,6 +11,7 @@ class SearchBar extends React.Component {
     return (
       <div className='search-bar'>
         <input 
+          placeholder='Search...'
           value={this.state.term}
           onChange={ event => this.onInputChange(event.target.value) } />
       </div>
